05bcb57dfbe4bdce57fb25ec4ffe87d97c2a8052
aidana <aidana251293@gmail.com>
2019-07-15 23:38:19 -0500

    changes Config name

--- src/test/java/utilities/DriverSetup.java
+++ src/test/java/utilities/DriverSetup.java
@@ -1,14 +1,9 @@
 package utilities;
 
-import com.google.common.base.Function;
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -20,7 +15,7 @@ public class DriverSetup {
 
     public static WebDriver getDriver() {
         if (driver == null) {
-            switch (Config.getProperty("browser")) {
+            switch (ConfigReader.getProperty("browser")) {
                 case "firefox":
                     WebDriverManager.firefoxdriver().setup();
                     driver = new FirefoxDriver();
