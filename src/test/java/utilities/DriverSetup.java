5d1d76b0ba21104971296d8a9c6d50a23af7f994
Nazar <shvednazar994@gmail.com>
2019-07-15 23:04:03 -0500

    Nazar

---
+++ src/test/java/utilities/DriverSetup.java
@@ -0,0 +1,52 @@
+package utilities;
+
+import com.google.common.base.Function;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
+
+public class DriverSetup {
+
+
+    private static WebDriver driver;
+
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            switch (Config.getProperty("browser")) {
+                case "firefox":
+                    WebDriverManager.firefoxdriver().setup();
+                    driver = new FirefoxDriver();
+                    break;
+                default:
+                    WebDriverManager.chromedriver().setup();
+                    driver = new ChromeDriver();
+            }
+
+            driver.manage().timeouts().implicitlyWait(12, TimeUnit.SECONDS);
+            driver.manage().window().maximize();
+
+        }
+        return driver;
+    }
+
+
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+
+
+    }
+
+
+}
