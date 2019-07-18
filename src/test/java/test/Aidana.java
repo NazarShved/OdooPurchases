fddb488f30a63b43cd3f5a81b57456b5286662ea
aidana <aidana251293@gmail.com>
2019-07-18 18:35:52 -0500

    Aidana's method

--- src/test/java/test/Aidana.java
+++ src/test/java/test/Aidana.java
@@ -1,7 +1,25 @@
 package test;
 
-public class Aidana {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
-    //my first comment
-    //my second comment
+import java.util.List;
+
+public class Aidana extends BaseTest {
+
+    //Test cases #156,162
+
+    @Test
+    public void sortIncomingProductsByStatus() throws InterruptedException {
+        purchases.switchTab("Incoming Products");
+        purchases.switchToSortTab("Status");
+        Thread.sleep(1000);
+        List<WebElement> statusValues = $$(By.xpath("//td[@class='o_data_cell o_readonly_modifier']"));
+        System.out.println(statusValues.size());
+        for (WebElement statusValue:statusValues) {
+
+            //System.out.println(statusValue.getText());
+        }
+    }
 }
