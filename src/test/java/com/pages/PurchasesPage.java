328afc533d2d4d491825a8d5dd27b9049fb792ec
Nazar <shvednazar994@gmail.com>
2019-07-18 03:47:46 -0500

    Added 1 more test case + some changes

--- src/test/java/com/pages/PurchasesPage.java
+++ src/test/java/com/pages/PurchasesPage.java
@@ -11,6 +11,8 @@ import java.util.List;
 public class PurchasesPage extends BasePage {
     public By searchField = byCss(".o_searchview_input");
     public By searchOptinsTab = byCss(".btn-group.o_search_options");
+    public By resultsInCanban = byCss(".oe_kanban_global_click.o_kanban_record");
+    public By resultsInList = byCss(".o_data_row");
 
     //Clicks on the inputed tab from the menu to the right CaseSensitive
     public void switchTab(String tab){
@@ -29,7 +31,9 @@ public class PurchasesPage extends BasePage {
         $(searchField).clear();
         $(searchField).sendKeys(input);
         $(searchField).sendKeys(Keys.ENTER);
-        wait.until(Conditions.listSizeIsAtLeast(byCss(".o_data_row"), 30));
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(byCss(".o_loading")));
+        if($$(resultsInCanban).size() == 0) wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(resultsInList));
+        else wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(resultsInCanban));
     }
 
     //opens website logs in, and navigates to purchases module
@@ -40,10 +44,17 @@ public class PurchasesPage extends BasePage {
         $get(By.className("oe_menu_text"),8).click();
 
         wait.until(Conditions.listSizeIsAtLeast(byCss(".o_data_row"), 50));
-        wait.until(ExpectedConditions.invisibilityOfElementLocated(byCss(".o_loading")));
     }
 
     public void showHideSearchFilters(){
         $("[title = 'Advanced Search...']").click();
+    }
+    public void resultsAsCanban(){
+        $("[aria-label='kanban']").click();
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(byCss(".o_loading")));
+    }
+    public void resultsAsList(){
+        $("[aria-label='list']").click();
+        wait.until(ExpectedConditions.invisibilityOfElementLocated(byCss(".o_loading")));
     }
 }
