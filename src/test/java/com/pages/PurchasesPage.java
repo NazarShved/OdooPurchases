f97f97df5299da944e7ed5f92bd4d3ccb132506a
Nurlan Isabekov <nurlanisa90@gmail.com>
2019-07-18 21:26:59 -0500

    my new code

--- src/test/java/com/pages/PurchasesPage.java
+++ src/test/java/com/pages/PurchasesPage.java
@@ -32,8 +32,13 @@ public class PurchasesPage extends BasePage {
         $(searchField).sendKeys(input);
         $(searchField).sendKeys(Keys.ENTER);
         wait.until(ExpectedConditions.invisibilityOfElementLocated(byCss(".o_loading")));
-        if($$(resultsInCanban).size() == 0) wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(resultsInList));
-        else wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(resultsInCanban));
+
+            if ($$(resultsInCanban).size() > 0)
+                wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(resultsInCanban));
+
+            else if($$(resultsInList).size() > 0)
+                wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(resultsInList));
+
     }
 
     //opens website logs in, and navigates to purchases module
--- src/test/java/test/Nurlan.java
+++ src/test/java/test/Nurlan.java
@@ -1,12 +1,37 @@
 package test;
 
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
+import org.openqa.selenium.JavascriptExecutor;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Nurlan extends BaseTest {
 
+    @Test(priority = 1)
+    public void checkSearchByPO()throws InterruptedException{
+
+    purchases.search(config.getProperty("PONumber"));
+
+    String actualPO = $x("(//td[@class='o_data_cell o_required_modifier'])[1]").getText();
+
+    String expectedPO = config.getProperty("PONumber");
+
+    Assert.assertEquals(actualPO,expectedPO, "No such PO number in the system");
+    }
 
 
+    @Test(priority = 2)
+
+    public void checkSearchByWrongPO(){
+
+        purchases.search(config.getProperty("note"));
+
+        String text = (String)((JavascriptExecutor)getWebDriver()).executeScript("return document.getElementsByClassName(\"oe_view_nocontent\")[0].textContent");
+
+        boolean expectedResult = text.contains("quotation is converted into a purchase order.");
+
+        Assert.assertTrue(expectedResult, "Text Changed");
+
+
+    }
 }
